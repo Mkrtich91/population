6e418848360595534c43f444315edc828739e122
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-06-16 13:12:58 +0400

    solved at 16.06.23mk

--- Population/Population.cs
+++ Population/Population.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace PopulationTask
 {
@@ -24,7 +24,41 @@ namespace PopulationTask
         /// <exception cref="ArgumentOutOfRangeException">Throw if the value of percents is less then 0% or more then 100%.</exception>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("Initial population must be greater than 0.");
+            }
+
+            if (percent < 0 || percent > 100)
+            {
+               throw new ArgumentOutOfRangeException(nameof(percent), "Percent value must be between 0 and 100.");
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("Visitor count cannot be less than 0.");
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("Current population must be greater than 0.");
+            }
+
+            if (currentPopulation <= initialPopulation)
+            {
+            throw new ArgumentException("Current population must be greater than initial population.");
+            }
+
+            int years = 0;
+            double population = initialPopulation;
+
+            while (population < currentPopulation)
+            {
+                population += (population * percent / 100) + visitors;
+                years++;
+            }
+
+            return years;
         }
     }
 }
